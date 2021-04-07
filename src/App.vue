ca8846d67132168abf7fa3c9808c866c7cf70762
Tianyang Gao <gaotianyang@sunseaaiot.com>
2021-04-07 14:33:34 +0800

    feat: 引入ts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Home from "./components/Home.vue";
 
-export default {
-  name: 'App',
+@Options({
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
